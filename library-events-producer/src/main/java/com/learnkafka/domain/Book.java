4eba874454acc3476d8e33b9dc8cd721c0617f6e
Đỗ Đức Duy <90464550+doducduy20127477@users.noreply.github.com>
2023-11-08 00:39:06 +0700

    Initial commit

---
+++ library-events-producer/src/main/java/com/learnkafka/domain/Book.java
@@ -0,0 +1,10 @@
+package com.learnkafka.domain;
+
+public record Book(
+        Integer BookId,
+        String bookName,
+        String bookAuthor
+
+) {
+
+}
---
+++ library-events-producer/src/main/java/com/learnkafka/domain/LibraryEvent.java
@@ -0,0 +1,12 @@
+package com.learnkafka.domain;
+
+
+
+
+public record LibraryEvent(
+        Integer libraryEventId,
+        LibraryEventType libraryEventType,
+        Book book
+) {
+
+}
