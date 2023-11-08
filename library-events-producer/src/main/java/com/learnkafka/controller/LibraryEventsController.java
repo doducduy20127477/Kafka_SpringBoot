31643bdd6fbd591a8663a61bcaf128e1cb2be7b3
doducduy20127477 <90464550+doducduy20127477@users.noreply.github.com>
2023-11-08 15:10:12 +0700

    Build Producer

--- library-events-producer/src/main/java/com/learnkafka/controller/LibraryEventsController.java
+++ library-events-producer/src/main/java/com/learnkafka/controller/LibraryEventsController.java
@@ -1,7 +1,9 @@
 package com.learnkafka.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.learnkafka.domain.LibraryEvent;
 import com.learnkafka.domain.LibraryEventType;
+import com.learnkafka.producer.LibraryEventsProducer;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
@@ -10,17 +12,31 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeoutException;
+
 import static org.apache.kafka.common.requests.FetchMetadata.log;
 
 @RestController
 @Slf4j
 public class LibraryEventsController {
+    public LibraryEventsProducer libraryEventsProducer;
+
+    public LibraryEventsController (LibraryEventsProducer libraryEventsProducer) {
+        this.libraryEventsProducer = libraryEventsProducer;
+    }
 
     @PostMapping("/v1/libraryevent")
     public ResponseEntity<LibraryEvent> postLibraryEvent(
             @RequestBody LibraryEvent libraryEvent
-            ) {
+            ) throws JsonProcessingException, ExecutionException, InterruptedException, TimeoutException {
+
         log.info("libraryevent: {}", libraryEvent);
+//        libraryEventsProducer.sendLibraryEvents(libraryEvent);
+//        libraryEventsProducer.sendLibraryEvents_approach2(libraryEvent);
+        libraryEventsProducer.sendLibraryEvents_approach3(libraryEvent);
+        log.info("After sending libraryEvents: ");
+
         return ResponseEntity.status(HttpStatus.CREATED).body(libraryEvent);
     }
 }
---
+++ library-events-producer/src/main/java/com/learnkafka/producer/LibraryEventsProducer.java
@@ -0,0 +1,93 @@
+package com.learnkafka.producer;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.learnkafka.domain.LibraryEvent;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.header.Header;
+import org.apache.kafka.common.header.internals.RecordHeader;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.stereotype.Component;
+
+import static java.util.List.of;
+import static org.apache.kafka.common.requests.FetchMetadata.log;
+
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+@Component
+@Slf4j
+public class LibraryEventsProducer {
+    public KafkaTemplate<Integer, String> kafkaTemplate;
+    @Value("${spring.kafka.topic}")
+    public String topic;
+
+    private final ObjectMapper objectMapper;
+
+    public LibraryEventsProducer (KafkaTemplate<Integer, String> kafkaTemplate, ObjectMapper objectMapper) {
+        this.kafkaTemplate = kafkaTemplate;
+        this.objectMapper = objectMapper;
+    }
+
+    public CompletableFuture<SendResult<Integer, String>> sendLibraryEvents(LibraryEvent libraryEvent) throws JsonProcessingException {
+        var key = libraryEvent.libraryEventId();
+        var value = objectMapper.writeValueAsString(libraryEvent);
+
+        var completableFuture =  kafkaTemplate.send(topic, key, value);
+        return completableFuture
+                .whenComplete((sendResult, throwable) -> {
+                    if (throwable != null) {
+                        handleFailure(key, value ,throwable);
+                    } else {
+                        handleSuccess(key, value ,sendResult);
+                    }
+                })
+        ;
+    }
+
+    public CompletableFuture<SendResult<Integer, String>> sendLibraryEvents_approach3(LibraryEvent libraryEvent) throws JsonProcessingException {
+        var key = libraryEvent.libraryEventId();
+        var value = objectMapper.writeValueAsString(libraryEvent);
+        var producerRecord = buildProducerRecord(key, value);
+        var completableFuture =  kafkaTemplate.send(producerRecord);
+        return completableFuture
+                .whenComplete((sendResult, throwable) -> {
+                    if (throwable != null) {
+                        handleFailure(key, value ,throwable);
+                    } else {
+                        handleSuccess(key, value ,sendResult);
+                    }
+                })
+                ;
+    }
+
+    private ProducerRecord<Integer, String> buildProducerRecord (Integer key, String value) {
+        List<Header> recordHeaders = List.of(new RecordHeader("event-soure", "scanner".getBytes()));
+        return new ProducerRecord<>(topic, null, key, value, recordHeaders);
+    }
+
+    public SendResult<Integer, String> sendLibraryEvents_approach2(LibraryEvent libraryEvent) throws JsonProcessingException, ExecutionException, InterruptedException, TimeoutException {
+        var key = libraryEvent.libraryEventId();
+        var value = objectMapper.writeValueAsString(libraryEvent);
+
+        var sendResult =  kafkaTemplate.send(topic, key, value).get(3, TimeUnit.SECONDS);
+        handleSuccess(key, value ,sendResult);
+        return sendResult;
+    }
+    private void handleSuccess (Integer key, String value, SendResult<Integer, String> sendResult) {
+        log.info("Message sent successfully for the key: {} and the value: {}, partition is {} ", key, value, sendResult.getRecordMetadata().partition());
+    }
+
+    private void handleFailure (Integer key, String value, Throwable throwable) {
+        log.error("Error sending the message and the exception is {} ", throwable.getMessage(), throwable);
+    }
+
+
+}
