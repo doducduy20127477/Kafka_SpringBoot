5dfb407064589aef176cca5986079b2bb96e8ad8
doducduy20127477 <90464550+doducduy20127477@users.noreply.github.com>
2023-11-09 15:50:32 +0700

    feat(consumer): consumer group and offset management

--- library-events-consumer/src/main/java/com/learnkafka/config/LibraryEventsConsumerConfig.java
+++ library-events-consumer/src/main/java/com/learnkafka/config/LibraryEventsConsumerConfig.java
@@ -1,10 +1,39 @@
 package com.learnkafka.config;
 
+import org.apache.catalina.Container;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.kafka.ConcurrentKafkaListenerContainerFactoryConfigurer;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.config.ContainerCustomizer;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
+import org.springframework.kafka.listener.ContainerProperties;
 
 @Configuration
 @EnableKafka
 public class LibraryEventsConsumerConfig {
+	
+	@Autowired
+	KafkaProperties kafkaProperties;
+	@Bean
+	ConcurrentKafkaListenerContainerFactory<?, ?> kafkaListenerContainerFactory(
+			ConcurrentKafkaListenerContainerFactoryConfigurer configurer,
+			ObjectProvider<ConsumerFactory<Object, Object>> kafkaConsumerFactory,
+			ObjectProvider<ContainerCustomizer<Object, Object, ConcurrentMessageListenerContainer<Object, Object>>> kafkaContainerCustomizer) {
+		ConcurrentKafkaListenerContainerFactory<Object, Object> factory = new ConcurrentKafkaListenerContainerFactory<>();
+		configurer.configure(factory, kafkaConsumerFactory
+				                              .getIfAvailable(() -> new DefaultKafkaConsumerFactory<>(this.kafkaProperties.buildConsumerProperties())));
+		kafkaContainerCustomizer.ifAvailable(factory::setContainerCustomizer);
+		factory.setConcurrency(3);
+//		factory.getContainerProperties().setAckMode(ContainerProperties.AckMode.MANUAL);
+		return factory;
+	}
 
 }
