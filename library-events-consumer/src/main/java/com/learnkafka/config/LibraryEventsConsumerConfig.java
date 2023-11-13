03e9927a055eb8f6b559bd732896e44d6e61ce44
doducduy20127477 <90464550+doducduy20127477@users.noreply.github.com>
2023-11-13 10:49:03 +0700

    feat(consumer): add retry for errorHandler

--- library-events-consumer/src/main/java/com/learnkafka/config/LibraryEventsConsumerConfig.java
+++ library-events-consumer/src/main/java/com/learnkafka/config/LibraryEventsConsumerConfig.java
@@ -1,5 +1,6 @@
 package com.learnkafka.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.catalina.Container;
 import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,16 +9,26 @@ import org.springframework.boot.autoconfigure.kafka.ConcurrentKafkaListenerConta
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.dao.RecoverableDataAccessException;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.config.ContainerCustomizer;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.listener.CommonErrorHandler;
 import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
 import org.springframework.kafka.listener.ContainerProperties;
+import org.springframework.kafka.listener.DefaultErrorHandler;
+import org.springframework.kafka.support.ExponentialBackOffWithMaxRetries;
+import org.springframework.util.backoff.ExponentialBackOff;
+import org.springframework.util.backoff.FixedBackOff;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
 @EnableKafka
+@Slf4j
 public class LibraryEventsConsumerConfig {
 	
 	@Autowired
@@ -32,8 +43,35 @@ public class LibraryEventsConsumerConfig {
 				                              .getIfAvailable(() -> new DefaultKafkaConsumerFactory<>(this.kafkaProperties.buildConsumerProperties())));
 		kafkaContainerCustomizer.ifAvailable(factory::setContainerCustomizer);
 		factory.setConcurrency(3);
+		factory.setCommonErrorHandler(errorHandler());
 //		factory.getContainerProperties().setAckMode(ContainerProperties.AckMode.MANUAL);
 		return factory;
+	}
+
+	private DefaultErrorHandler errorHandler () {
+		var exceptionToIgnoreList = List.of(
+				IllegalAccessException.class
+		);
+		var exceptionToRetryList = List.of(
+				RecoverableDataAccessException.class
+		);
+		var fixedBackOff = new FixedBackOff(1000L, 2);
+		var expBackOff = new ExponentialBackOffWithMaxRetries(2);
+		expBackOff.setInitialInterval(1_000L);
+		expBackOff.setMultiplier(2.0);
+		expBackOff.setMaxInterval(2_000L);
+		DefaultErrorHandler errorHandler = new DefaultErrorHandler(
+				//fixedBackOff
+				expBackOff
+		);
+//		exceptionToIgnoreList.forEach(errorHandler:: addNotRetryableExceptions);
+		exceptionToRetryList.forEach(errorHandler:: addRetryableExceptions);
+		errorHandler
+				.setRetryListeners((record, ex, deliveryAttempt) -> {
+					log.info("Failed Record in Retry Listener, Exception: {} , deliveryAttempt: {} "
+							, ex.getMessage(), deliveryAttempt);
+				});
+		return errorHandler;
 	}
 
 }
